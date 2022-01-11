90ab336bdb148a496c8047c6a48fa2b1a3592f09
tgiannotta <t.giannotta92@gmail.com>
2022-01-11 15:07:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <Header /> 
+      <main>
+      <h2>I contenuti andranno qui</h2>
+   </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./style/general.scss";
+#app {
+    main{
+     
+    color: orange;
+    background-color: black;
+    height: 200px;
+    
+  }
+  
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <div class="logo">
+                <img src="../assets/img/logo-dc.png" alt="Logo DC">
+            </div>
+            <nav>
+                <ul class="menu">
+                            <li>
+                                <a href="#">Personaggi</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movie</a>
+                            </li>
+                            <li>
+                                <a class="current" href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Collectables</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">Fans</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop</a>
+                            </li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name:'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+header{
+    
+
+    .wrapper{
+        height: 120px;
+        display: flex;
+        justify-content:space-between;
+        align-items: center;
+    
+
+        /* Logo */
+         .logo{
+            img{
+                width: 100px;
+             }
+        }
+
+        /* menu */
+        .menu{
+            display: flex;
+            li{
+                margin-right: 50px;
+                a{
+                    display: inline-block;
+                    color: black;
+                    text-decoration: none;
+                    font-size: 18px;
+                    text-transform: uppercase;
+                    font-weight: bold;
+                    line-height: 115px;
+                    &.current{
+                        color: $colore-testi-secondario;
+                        border-bottom: 5px solid $colore-testi-secondario;
+                        
+                    }
+                }
+            }
+        }
+    }
+   
+}
+</style>
